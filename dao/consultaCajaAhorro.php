7405e2555472535f0b9540d9cf3c803e20bd669a
hadbet <hadbet.altamirano@grammer.com>
2025-01-07 13:23:52 -0600

    Carga Inicial del sistema

--- dao/consultaCajaAhorro.php
+++ dao/consultaCajaAhorro.php
@@ -7,7 +7,7 @@ function ContadorApu()
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    $datos = mysqli_query($conex, "SELECT * FROM `CajaAhorro`");
+    $datos = mysqli_query($conex, "SELECT * FROM `AuxCajaAhorro`");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
